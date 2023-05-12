3915b3bb788ecbf068a67e1e9ed11a1d9241773c
Ivan Mauricio Marin Lombo <black_death_metalero@outlook.com>
2023-05-12 12:14:06 -0500

    Cambio Cadena de Conexion

--- StGeneticsAPI/Properties/Settings.Designer.cs
+++ StGeneticsAPI/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace StGeneticsAPI.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=METAL-ZONE;Initial Catalog=STGenetics;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=lago.cunapp.dev;Initial Catalog=STGenetics;Persist Security Info=True" +
+            ";User ID=ivan_marin;Password=Kreator666")]
         public string StringConnection {
             get {
                 return ((string)(this["StringConnection"]));
